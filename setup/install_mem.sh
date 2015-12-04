e521e3b876d77e9b483721e3e34b8ea1ad5a79e3
Joosep Pata <joosep.pata@cern.ch>
2015-12-04 22:11:19 +0200

    added MEM installation code, moved files to interface/

---
+++ setup/install_mem.sh
@@ -0,0 +1,14 @@
+#source this script to install the MEM package
+cd $CMSSW_BASE
+
+#make the MEM install dir
+mkdir -p src/TTH
+cd src/TTH
+
+#get the MEM code
+git clone https://github.com/bianchini/Code.git MEIntregratorStandalone
+
+#copy the OpenLoops ME libraries
+cp $CMSSW_BASE/src/TTH/MEIntegratorStandalone/libs/*.so $CMSSW_BASE/lib/$SCRAM_ARCH/
+
+cd $CMSSW_BASE
