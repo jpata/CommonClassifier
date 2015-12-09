7b453f172920d5fb5214a59e375610231e0968ff
Hannes Mildner <hannes.mildner@cern.ch>
2015-12-09 15:31:32 +0100

    added actual BDTClassifier header

--- interface/BDTClassifier.h
+++ interface/BDTClassifier.h
@@ -1,30 +1,50 @@
-#ifndef BDTCLASSIFIER_H
-#define BDTCLASSIFIER_H
+#ifndef TTH_BDTCLASSIFIER_H
+#define TTH_BDTCLASSIFIER_H
+#include <vector>
+#include <map>
+#include <math.h> 
+#include "TLorentzVector.h"
+#include "TMVA/Reader.h"
+#include "TTH/CommonClassifier/interface/BDTvars.h"
 
-class CommonClassifier{
+
+// class to evaluate lepton plus jets BDT set
+class BDTClassifier{
   
 public:
-  BDTClassifier();
-  ~BDTClassifier();
+
+    BDTClassifier(std::string weightpath="");
+    ~BDTClassifier();
+
+    // Call this method to return the BDT output, provide all necessary inputs. Jet CSV should be sorted the same way as jet p4. 
+    // We could also write a class to contain the jet CSV and p4 information
+    double GetBDTOutput(const std::vector<TLorentzVector>& selectedLeptonP4, 
+			const std::vector<TLorentzVector>& selectedJetP4, 
+			const std::vector<double>& selectedJetCSV, 
+			const std::vector<TLorentzVector>& looseSelectedJetP4, 
+			const std::vector<double>& looseSelectedJetCSV, 
+			const TLorentzVector& metP4);
+  
+    // returns the category of the last evaluated Event
+    std::string GetCategoryOfLastEvaluation() const;
+    
+    // return the variable names and their values for the last evaluated event
+    std::map<std::string,float> GetVariablesOfLastEvaluation() const;
 
 
-  // Call this method to return the BDT output, provide all necessary inputs. Jet CSV should be sorted the same way as jet p4. 
-  // We could also write a class to contain the jet CSV and p4 information
-  double GetBDTOutput(const std::vector<TLorentzVector>& selectedLeptonP4, const std::vector<TLorentzVector>& selectedJetP4, const std::vector<double>& selectedJetCSV, const std::vector<TLorentzVector>& looseSelectedJetP4, const std::vector<double>& looseSelectedJetCSV, TLorentzVector& metP4);
-  
-  // returns the category of the last evaluated Event
-  std::string GetCategoryOfLastEvaluation() const;
-  
-  // return the variable names and their values for the last evaluated event
-  std::map<std::string,double> GetVariablesOfLastEvaluation() const;
-  
-  
-private:
-   //map holding the various BDT input variables
-   std::map<std::string,double> variableMap;
-  
-   std::string categoryOfLastEvaluation;
-   
+private:  
+    void SetCategory(const std::vector<TLorentzVector>& selectedLeptonP4, 
+		     const std::vector<TLorentzVector>& selectedJetP4, 
+		     const std::vector<double>& selectedJetCSV);
+    void ResetVariableMap();
+
+    const double btagMcut;
+    
+    std::string category;
+    std::map<std::string,TMVA::Reader*> readerMap;
+    std::map<std::string,float> variableMap;
+    BDTvars bdtvar;
+
 };
 
 #endif
