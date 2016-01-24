1dd0b7663e4388c0157b6ae04fa2c537918dbd62
Karim <uvclu@student.kit.edu>
2016-01-24 20:57:10 +0100

    added class for BDT with MEM_p as input in the 4tag categories

--- src/classes.h
+++ src/classes.h
@@ -1,6 +1,8 @@
 #include "TTH/CommonClassifier/interface/MEMClassifier.h"
 #include "TTH/CommonClassifier/interface/BDTClassifier.h"
 #include "TTH/CommonClassifier/interface/BlrBDTClassifier.h"
+#include "TTH/CommonClassifier/interface/MemBDTClassifier.h"
+
 namespace {
     namespace {
         std::vector<TLorentzVector> _v1;
