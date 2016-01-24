51eb05c09645144fc18a186302607f9653fa9663
Karim <uvclu@student.kit.edu>
2016-01-24 18:54:19 +0100

    added a new class for BDT with blr as input variable

--- src/classes.h
+++ src/classes.h
@@ -1,5 +1,6 @@
 #include "TTH/CommonClassifier/interface/MEMClassifier.h"
 #include "TTH/CommonClassifier/interface/BDTClassifier.h"
+#include "TTH/CommonClassifier/interface/BlrBDTClassifier.h"
 
 namespace {
     namespace {
