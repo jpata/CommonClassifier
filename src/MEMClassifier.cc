85893dbd24ddacb1f0246d9cda5a1b46cd04b1c9
Gregor Kasieczka <gregor.kasieczka@cern.ch>
2016-02-03 18:51:52 +0100

    MEM for running on subjets

--- src/MEMClassifier.cc
+++ src/MEMClassifier.cc
@@ -3,10 +3,12 @@
 static const double mem_weight = 0.15;
 
 void MEMClassifier::setup_mem(
+    const Hypothesis hypo,
     const std::vector<TLorentzVector>& selectedLeptonP4,
     const std::vector<double>& selectedLeptonCharge,
     const std::vector<TLorentzVector>& selectedJetP4,
     const std::vector<double>& selectedJetCSV,
+    const std::vector<JetType>& selectedJetType,
     const std::vector<TLorentzVector>& looseSelectedJetP4,
     const std::vector<double>& looseSelectedJetCSV,
     TLorentzVector& metP4,
@@ -23,6 +25,66 @@ void MEMClassifier::setup_mem(
       MEM::Permutations::QQbarBBbarSymmetry
     });
 
+    switch (hypo){
+    
+    case DEBUG:
+      {
+      break;
+      }
+            
+    case SL_0W2H2T:
+      {	
+	setup_mem_sl_0w2h2t(selectedLeptonP4,
+			    selectedLeptonCharge,
+			    selectedJetP4,
+			    selectedJetCSV,
+			    selectedJetType,
+			    looseSelectedJetP4,
+			    looseSelectedJetCSV,
+			    metP4,
+			    objs,
+			    res);
+	break;
+      }
+
+    case SL_2W2H2T_SJ:
+      {
+	setup_mem_sl_2w2h2t_sj(selectedLeptonP4,
+			       selectedLeptonCharge,
+			       selectedJetP4,
+			       selectedJetCSV,
+			       selectedJetType,
+			       looseSelectedJetP4,
+			       looseSelectedJetCSV,
+			       metP4,
+			       objs,
+			       res);
+	break;
+      }
+
+    // Fallback
+    default:
+      {
+	std::cout << "Warning! Fallback case reached. Invalid MEM Hypo!!!" << std::endl;      
+      }
+      
+    }
+       
+}
+
+
+void MEMClassifier::setup_mem_sl_0w2h2t(
+    const std::vector<TLorentzVector>& selectedLeptonP4,
+    const std::vector<double>& selectedLeptonCharge,
+    const std::vector<TLorentzVector>& selectedJetP4,
+    const std::vector<double>& selectedJetCSV,
+    const std::vector<JetType>& selectedJetType,
+    const std::vector<TLorentzVector>& looseSelectedJetP4,
+    const std::vector<double>& looseSelectedJetCSV,
+    TLorentzVector& metP4,
+    std::vector<MEM::Object*>& objs,
+    MEMResult& res
+    ) {
 
     if (selectedLeptonP4.size() != 1) {
         throw std::runtime_error("Expected a single-lepton event");
@@ -52,7 +114,8 @@ void MEMClassifier::setup_mem(
 
         MEM::Object* jet = make_jet(
             p4.Pt(), p4.Eta(), p4.Phi(), p4.M(), is_btagged ? 1.0 : 0.0,
-            selectedJetCSV.at(ij)
+            selectedJetCSV.at(ij),
+	    false
         );
         if (is_btagged) {
             tagged.push_back(jet);
@@ -69,11 +132,106 @@ void MEMClassifier::setup_mem(
         std::cout << "adding jet " << jet->p4().Pt() << " btag " << jet->getObs(MEM::Observable::BTAG) << std::endl;
     }
 
-    //Ignore the untagged jets
-    //for (auto* jet : untagged) {
-    //    objs.push_back(jet);
-    //    integrand->push_back_object(jet);
-    //}
+    for (unsigned int il=0; il < selectedLeptonP4.size(); il++) {
+        TLorentzVector lep_p4 = selectedLeptonP4.at(il);
+        assert(lep_p4.Pt() > 0);
+        MEM::Object* lep = make_lepton(lep_p4.Pt(), lep_p4.Eta(), lep_p4.Phi(), lep_p4.M(), selectedLeptonCharge[il]);
+        objs.push_back(lep);
+        integrand->push_back_object(lep);
+    }
+
+    assert(metP4.Pt() > 0);
+    MEM::Object met(metP4, MEM::ObjectType::MET );
+    integrand->push_back_object(&met);
+}
+
+
+void MEMClassifier::setup_mem_sl_2w2h2t_sj(
+    const std::vector<TLorentzVector>& selectedLeptonP4,
+    const std::vector<double>& selectedLeptonCharge,
+    const std::vector<TLorentzVector>& selectedJetP4,
+    const std::vector<double>& selectedJetCSV,
+    const std::vector<JetType>& selectedJetType,
+    const std::vector<TLorentzVector>& looseSelectedJetP4,
+    const std::vector<double>& looseSelectedJetCSV,
+    TLorentzVector& metP4,
+    std::vector<MEM::Object*>& objs,
+    MEMResult& res
+    ) {
+
+    if (selectedLeptonP4.size() != 1) {
+        throw std::runtime_error("Expected a single-lepton event");
+    }
+    
+    // Make sure we received a suitable set of input jets
+    // Require: 
+    //  * exactly 6 jets
+    //  * 2 light boosted
+    //  * 1 b boosted
+    //  * 3 resolved
+    
+    int n_light_boosted(0), n_b_boosted(0), n_resolved(0);
+
+    assert(selectedJetP4.size() == 6);
+                
+    std::vector<MEM::Object*> tagged;
+    std::vector<MEM::Object*> untagged;
+
+    for (unsigned int ij=0; ij<selectedJetP4.size(); ij++) {    
+
+      TLorentzVector p4 = selectedJetP4.at(ij);
+      assert(p4.Pt() > 0);
+      
+      switch (selectedJetType[ij]){
+	
+      case RESOLVED:
+	{
+	  MEM::Object* jet = make_jet(
+				      p4.Pt(), p4.Eta(), p4.Phi(), p4.M(), 1.0,
+				      selectedJetCSV.at(ij),
+				      false
+				      );
+	  
+	  tagged.push_back(jet);
+	  n_resolved++;
+	  break;
+	}
+
+      case BOOSTED_LIGHT:
+	{
+	  MEM::Object* jet = make_jet(
+				      p4.Pt(), p4.Eta(), p4.Phi(), p4.M(), 0.0,
+				      selectedJetCSV.at(ij),
+				      true
+				      );
+	  untagged.push_back(jet);
+	  n_light_boosted++;
+	  break;
+	}
+
+      case BOOSTED_B:
+	{
+	  MEM::Object* jet = make_jet(
+				      p4.Pt(), p4.Eta(), p4.Phi(), p4.M(), 1.0,
+				      selectedJetCSV.at(ij),
+				      true
+				      );
+	  tagged.push_back(jet);
+	  n_b_boosted++;
+	  break;
+	}
+      }
+    }
+
+    assert(n_light_boosted==2);
+    assert(n_b_boosted==1);	
+    assert(n_resolved==3);
+
+    for (auto* jet : tagged) {
+        objs.push_back(jet);
+        integrand->push_back_object(jet);
+        std::cout << "adding jet " << jet->p4().Pt() << " btag " << jet->getObs(MEM::Observable::BTAG) << std::endl;
+    }
 
     for (unsigned int il=0; il < selectedLeptonP4.size(); il++) {
         TLorentzVector lep_p4 = selectedLeptonP4.at(il);
@@ -88,41 +246,86 @@ void MEMClassifier::setup_mem(
     integrand->push_back_object(&met);
 }
 
+
 MEMResult MEMClassifier::GetOutput(
+    const Hypothesis hypo,
     const std::vector<TLorentzVector>& selectedLeptonP4,
     const std::vector<double>& selectedLeptonCharge,
     const std::vector<TLorentzVector>& selectedJetP4,
     const std::vector<double>& selectedJetCSV,
+    const std::vector<JetType>& selectedJetType,
     const std::vector<TLorentzVector>& looseSelectedJetP4,
     const std::vector<double>& looseSelectedJetCSV,
     TLorentzVector& metP4
 ) {
+  
+    // Make sure vector sizes match up
+    assert(selectedLeptonP4.size() == selectedLeptonCharge.size());
+    assert(selectedJetP4.size() == selectedJetCSV.size());
+    assert(selectedJetP4.size() == selectedJetType.size());
+    assert(looseSelectedJetP4.size() == looseSelectedJetCSV.size());
+  
     std::vector<MEM::Object*> objs;
     
     MEMResult res;
 
     setup_mem(
+        hypo,
         selectedLeptonP4,
         selectedLeptonCharge,
         selectedJetP4,
         selectedJetCSV,
+	selectedJetType,
         looseSelectedJetP4,
         looseSelectedJetCSV,
         metP4, objs, res
     );
-    std::cout << "MEM running signal" << std::endl;
-    MEM::MEMOutput res_sig = integrand->run(
-        MEM::FinalState::LH, MEM::Hypothesis::TTH, {}, {
-            MEM::PSVar::cos_q1, MEM::PSVar::phi_q1, MEM::PSVar::cos_qbar1, MEM::PSVar::phi_qbar1
-        }
-    );
 
-    std::cout << "MEM running background" << std::endl;
-    MEM::MEMOutput res_bkg = integrand->run(
-        MEM::FinalState::LH, MEM::Hypothesis::TTBB, {}, {
-            MEM::PSVar::cos_q1, MEM::PSVar::phi_q1, MEM::PSVar::cos_qbar1, MEM::PSVar::phi_qbar1
-        }
-    );
+    MEM::MEMOutput res_sig;
+    MEM::MEMOutput res_bkg;
+
+    switch (hypo){
+      
+    // DEBUG
+    case DEBUG:
+      {
+	std::cout << "Debug mode - not running MEM" << std::endl;
+	break;
+      }
+
+    // Single Lepton - Boosted 222
+    case SL_2W2H2T_SJ:
+      {
+	std::cout << "MEM running signal" << std::endl;
+	res_sig = integrand->run(MEM::FinalState::LH, MEM::Hypothesis::TTH, {}, {});
+	
+	std::cout << "MEM running background" << std::endl;
+	res_bkg = integrand->run(MEM::FinalState::LH, MEM::Hypothesis::TTBB, {}, {});
+	
+	break;
+      }
+
+    // Single Lepton - Resolved 022
+    case SL_0W2H2T:
+      {
+	std::cout << "MEM running signal" << std::endl;
+	res_sig = integrand->run(MEM::FinalState::LH, MEM::Hypothesis::TTH, {}, 
+				 {MEM::PSVar::cos_q1, MEM::PSVar::phi_q1, MEM::PSVar::cos_qbar1, MEM::PSVar::phi_qbar1});
+	
+	std::cout << "MEM running background" << std::endl;
+	res_bkg = integrand->run(MEM::FinalState::LH, MEM::Hypothesis::TTBB, {}, 
+				 {MEM::PSVar::cos_q1, MEM::PSVar::phi_q1, MEM::PSVar::cos_qbar1, MEM::PSVar::phi_qbar1});
+	
+	break;
+      }
+
+    // Fallback
+    default:
+      {
+	std::cout << "Warning! Fallback case reached. Invalid MEM Hypo!!!" << std::endl;      
+      }
+    }
+    
     for(auto* o : objs) {
         delete o;
     }
@@ -138,7 +341,7 @@ MEMResult MEMClassifier::GetOutput(
     return res;
 }
   
-MEM::Object* MEMClassifier::make_jet(double pt, double eta, double phi, double mass, double istagged, double csv) const {
+MEM::Object* MEMClassifier::make_jet(double pt, double eta, double phi, double mass, double istagged, double csv, bool is_subjet) const {
     TLorentzVector lv;
     lv.SetPtEtaPhiM(pt, eta, phi, mass);
     MEM::Object* obj = new MEM::Object(lv, MEM::ObjectType::Jet);
@@ -146,8 +349,14 @@ MEM::Object* MEMClassifier::make_jet(double pt, double eta, double phi, double m
     obj->addObs( MEM::Observable::CSV, csv); //b-tagger
     obj->addObs( MEM::Observable::PDGID, 0);  // currently not used
     // attach the transfer functions corresponding to the jet
-    obj->addTransferFunction(MEM::TFType::bReco, getTransferFunction("b", lv.Eta()));
-    obj->addTransferFunction(MEM::TFType::qReco, getTransferFunction("l", lv.Eta()));
+    if (is_subjet){
+      obj->addTransferFunction(MEM::TFType::bReco, getTransferFunction("sjb", lv.Eta()));
+      obj->addTransferFunction(MEM::TFType::qReco, getTransferFunction("sjl", lv.Eta()));
+    }
+    else{
+      obj->addTransferFunction(MEM::TFType::bReco, getTransferFunction("b", lv.Eta()));
+      obj->addTransferFunction(MEM::TFType::qReco, getTransferFunction("l", lv.Eta()));
+    }
     return obj;
 }
 
